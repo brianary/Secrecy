78c99df92c68917ab721c4b33d0b3d24895bd73d
Brian Lalonde <brianary@gmail.com>
2026-05-18 21:40:22 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿Secrecy
+=======
+
+<!-- To publish to PowerShell Gallery, commit an update to the .psd1 file -->
+<img src="images/Secrecy.svg" alt="Secrecy icon" align="right" height="200" width="200" />
+
+[![PowerShell Gallery Version](https://img.shields.io/powershellgallery/v/Secrecy)](https://www.powershellgallery.com/packages/Secrecy/)
+[![PowerShell Gallery](https://img.shields.io/powershellgallery/dt/Secrecy)](https://www.powershellgallery.com/packages/Secrecy/)
+[![Actions Status](https://github.com/brianary/Secrecy/actions/workflows/continuous.yml/badge.svg)](https://github.com/brianary/Secrecy/actions/workflows/continuous.yml)
+[![Mastodon: @dataelemental@mastodon.social](https://badgen.net/badge/@dataelemental/@mastodon.social/blue?icon=mastodon)](https://mastodon.social/@dataelemental "DataElemental Mastodon profile")
+
+A collection of general-purpose functions for objects, properties, and more.
+
+<img src="images/demo.gif" alt="a demonstration of the module commands" height="300" width="600" />
+
---
+++ src/public/Save-Secret.ps1
@@ -0,0 +1,75 @@
+<#
+.SYNOPSIS
+Sets a secret in a secret vault with metadata.
+
+.FUNCTIONALITY
+Credential
+
+.EXAMPLE
+Save-Secret.ps1 GitHubToken -Paste securestring -Title 'PowerShell token' -Description 'A GitHub classic token' -Url https://github.com/settings/tokens -Expires (Get-Date).AddDays(90)
+
+Stores the token from the clipboard.
+#>
+
+#Requires -Version 7
+#Requires -Modules Microsoft.PowerShell.SecretManagement,Microsoft.PowerShell.SecretStore
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText','',
+Justification='The data source is plaintext. SecureString benefits may be in dispute: <https://github.com/dotnet/platform-compat/blob/master/docs/DE0001.md>')]
+[CmdletBinding()] Param(
+# Specifies the name of the secret to add metadata to. Wildcard characters (`*`) are not permitted.
+[Parameter(Position=0,Mandatory=$true)][string] $Name,
+# Specifies the value of the secret.
+[Parameter(ParameterSetName='Secret',Position=1,Mandatory=$true)][securestring] $Secret,
+# Specifies the value of the credential to store.
+[Parameter(ParameterSetName='Credential',Position=1,Mandatory=$true)][pscredential] $Credential,
+# Title metadata field.
+[string] $Title,
+# Description metadata field.
+[string] $Description,
+# Note metadata field.
+[string] $Note,
+# Uri metadata field.
+[uri] $Uri,
+# Created date/time metadata field.
+[datetime] $Created = (Get-Date),
+# Expiration date/time metadata field.
+[datetime] $Expires,
+# Specifies the type to interpret the text on the clipboard as for use as the secret value.
+[Parameter(ParameterSetName='Paste',Mandatory=$true)]
+[ValidateSet('string','securestring','bytes','hexbytes')][string] $Paste,
+# Specifies the username to combine with the clipboard text as a password to store as a credential secret.
+[Parameter(ParameterSetName='PasteForUser',Mandatory=$true)][string] $PasteForUser,
+# Specifies the encoding to read the clipboard as, into a byte array secret.
+[Parameter(ParameterSetName='PasteTextBytes',Mandatory=$true)][Text.Encoding] $PasteTextBytes
+)
+$clipboard = Get-Clipboard |Out-String
+$value = switch($PSCmdlet.ParameterSetName)
+{
+    PasteForUser {New-Object pscredential $PasteForUser,($clipboard |ConvertTo-SecureString -AsPlainText -Force)}
+    PasteTextBytes {$PasteTextBytes.GetBytes($clipboard)}
+    Secret {$Secret}
+    Credential {$Credential}
+    Paste 
+    {
+        switch($Paste)
+        {
+            string {$clipboard}
+            securestring {$clipboard |ConvertTo-SecureString -AsPlainText -Force}
+            bytes {[byte[]]($clipboard -split '\D+')}
+            hexbytes {
+                $clipboard = $clipboard -replace '[^A-Fa-f0-9]+'
+                $bytes = [bigint]::Parse("00$clipboard",'HexNumber').ToByteArray()
+                [array]::Reverse($bytes)
+                $null,$bytes = $bytes
+                $bytes
+            }
+        }
+    }
+}
+$metadata = @{ Created = $Created }
+if($Title) {$metadata['Title'] = $Title}
+if($Description) {$metadata['Description'] = $Description}
+if($Note) {$metadata['Note'] = $Note}
+if($Uri) {$metadata['Uri'] = "$Uri"}
+if($Expires) {$metadata['Expires'] = $Expires}
+Set-Secret $Name $value -Metadata $metadata
