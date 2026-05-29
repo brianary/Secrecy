57027d25d0c37531fa4804aaea9cbd3ea38cec29
Brian Lalonde <brianary@gmail.com>
2026-05-28 20:20:19 -0700

    ✅ Debug test

--- test/Export-SecretVault.Tests.ps1
+++ test/Export-SecretVault.Tests.ps1
@@ -8,7 +8,8 @@ if((Test-Path .changes -Type Leaf) -and
 		Where-Object {$_.StartsWith("$(($MyInvocation.MyCommand.Name -split '\.',2)[0]).")})) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
-	Get-Module -ListAvailable |Out-String |Write-Information
+	Get-Module -ListAvailable |Out-File output.txt utf8BOM
+	'---' |Out-File output.txt utf8BOM -Append
 	$module = Join-Path ($PSScriptRoot |Split-Path) src .publish *.psd1 |Get-Item
 	$manifest = Test-ModuleManifest $module.FullName
 	if($manifest.RequiredModules)
@@ -18,6 +19,7 @@ BeforeAll {
 			Import-Module $_
 		}
 	}
+	Get-Module -ListAvailable |Out-File output.txt utf8BOM -Append
 	Import-Module $module -Force
 }
 Describe 'Export-SecretVault' -Tag Export-SecretVault {
