cbdbe3e736260f82015eaa7f29aaa88dab9b5aa8
Brian Lalonde <brianary@gmail.com>
2026-05-30 12:05:30 -0700

    ✨ Add cmdlet to set up secrets

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ Secret storage manipulation utilities.
 - [Export-SecretVault](https://github.com/brianary/Secrecy/wiki/Export-SecretVault): Exports secret vault content.
 - [Get-SecretDetails](https://github.com/brianary/Secrecy/wiki/Get-SecretDetails): Returns secret info from the secret vaults, including metadata as properties.
 - [Import-SecretVault](https://github.com/brianary/Secrecy/wiki/Import-SecretVault): Imports secrets into secret vaults.
+- [Initialize-SecretVault](https://github.com/brianary/Secrecy/wiki/Initialize-SecretVault): Sets up secret storage for the first time.
 - [Set-SecretDetails](https://github.com/brianary/Secrecy/wiki/Set-SecretDetails): Sets a secret in a secret vault with metadata.
---
+++ src/public/Initialize-SecretVault.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+Sets up secret storage for the first time.
+
+.FUNCTIONALITY
+Credential
+
+.LINK
+https://devblogs.microsoft.com/powershell/secretmanagement-and-secretstore-are-generally-available/
+
+.EXAMPLE
+Initialize-SecretVault
+
+
+#>
+
+#Requires -Version 7
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='High')] Param(
+# Disable password prompts
+[Alias('Headless')][switch] $HandsFree
+)
+if(Test-SecretVault) {Write-Information 'Secret vault is already set up.'; return}
+if(!$HandsFree) {Set-SecretStoreConfiguration -Default}
+else
+{
+	if(!$PSCmdlet.ShouldProcess('secret vault','set up without a password')) {return}
+	Set-SecretStoreConfiguration -Authentication None -Interaction None
+}
+Register-SecretVault -Name SecretVault -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+if(Test-SecretVault) {Write-Information 'Secret vault has been set up!'}
