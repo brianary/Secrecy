759ba4689abd76769f6c4a07b9c526f84bc94ab1
Brian Lalonde <brianary@gmail.com>
2026-05-31 20:01:02 -0700

    💚 Clean up module import script

--- scripts/Import-ThisModule.ps1
+++ scripts/Import-ThisModule.ps1
@@ -5,7 +5,6 @@ Imports this repository's module, installing and importing required modules firs
 
 #Requires -Version 7
 [CmdletBinding()] Param()
-Write-Information "Importing..."
 if(!($PSScriptRoot |Split-Path |Join-Path -ChildPath .publish |Test-Path -Type Container))
 {
 	$name = $PSScriptRoot |Split-Path |Join-Path -ChildPath src -AdditionalChildPath *.psd1 |Split-Path -LeafBase
@@ -14,27 +13,14 @@ if(!($PSScriptRoot |Split-Path |Join-Path -ChildPath .publish |Test-Path -Type C
 }
 $module = $PSScriptRoot |Split-Path |Join-Path -ChildPath .publish -AdditionalChildPath *.psd1 |Get-Item
 $manifest = Import-PowerShellDataFile $module.FullName
-Write-Information "Loading $module"
 if($manifest.ContainsKey('RequiredModules') -and $manifest['RequiredModules'])
 {
-	Write-Output "::group::Found required modules: $($manifest.RequiredModules -join ', ')"
-	try
-	{
-		$manifest.RequiredModules |ForEach-Object {
-			Write-Output "::notice::Module '$_' is required"
-			if(!(Get-Module $_ -ListAvailable -wa Ignore))
-			{
-				Write-Output "::notice::Installing '$_'"
-				Install-PSResource $_ -Scope CurrentUser -Repository PSGallery -TrustRepository
-			}
-			Import-Module $_
+	$manifest.RequiredModules |ForEach-Object {
+		if(!(Get-Module $_ -ListAvailable -wa Ignore))
+		{
+			Install-PSResource $_ -Scope CurrentUser -Repository PSGallery -TrustRepository
 		}
+		Import-Module $_
 	}
-	catch {Write-Output "::error::Trouble loading required modules: $_"}
-	Write-Output "::endgroup::"
-}
-else
-{
-	Write-Output "::notice::No required modules"
 }
 Import-Module $module -Force
